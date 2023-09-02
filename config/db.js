abbca25e6204f9c8baf530f762add5895b4fefdd
emre <emre_ozp@hotmail.com>
2023-09-02 15:10:26 +0300

    db connection complated

---
+++ config/db.js
@@ -0,0 +1,8 @@
+const pg = require("pg");
+const dotenv = require("dotenv").config();
+
+const postgresClient = new pg.Pool({
+    connectionString:process.env.DB_CONNECTION_STRING
+});
+
+module.exports = postgresClient
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const postgresClient = require("./config/db");
 const app = express();
 app.use(express.json());
 
-app.use("/car",async (req,res) => {
+app.use("/user",async (req,res) => {
     return res.json("connection success")
 })
 
-const port = process.env.PORT || 5000;
 
+const port = process.env.PORT || 5000;
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
+    postgresClient.connect(err => {
+        if(err){
+            console.log("Db Connection Error!",err.stack)
+        }else{
+            console.log("Db Connection Uccesful");
+        }
+    })
 })
